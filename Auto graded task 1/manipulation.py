a77103c8a6d41d42b15db1627ab8cb20be24041f
mattyb1217 <163048491+mattyb1217@users.noreply.github.com>
2024-03-14 17:54:47 +0000

    Add files via upload

---
+++ Auto graded task 1/manipulation.py
@@ -0,0 +1,17 @@
+# Create Sentence using input()
+# Save sentence as str_manip
+
+str_manip= input("My favourite meat is beef")
+print(str_manip)
+
+# calculting length of sentence using len()
+print (len("My favourite meat is beef"))
+
+#Finding last letter in sentence 
+ # last_letter = str_manip[-1]
+
+#and replace every of that letter with @
+#print(str_manip.replace(last_letter,"@"))
+
+# 5 letter word first 3 last 2
+#print(str_manip[:3] + str_manip[-2:])
---
+++ Auto graded task 2/numbers.py
@@ -0,0 +1,35 @@
+# # Ask for three integers
+# numbers= input("Can you enter three integers")
+# print(numbers)
+
+num1=6
+num2=7
+num3=8
+
+# calculate the sum of the numbers
+int(num1)
+int(num2)
+int(num3)
+
+print(int(num1))
+print(int(num2))
+print(int(num3))
+
+numbers= [6,7,8]
+total=0
+
+for number in numbers:
+    total+= number
+print(total) 
+
+
+# # take third number away from the first
+print(num1- num3)
+
+# # multiply first and third number
+print(num1*num3) 
+
+# sum the numbers and divide by third number
+print(total)
+print(total/num3)
+
