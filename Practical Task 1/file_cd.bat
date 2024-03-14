c199d0f4b5eb00f714df15a68a868aa546492257
mattyb1217 <163048491+mattyb1217@users.noreply.github.com>
2024-03-14 17:53:25 +0000

    Add files via upload

---
+++ Practical Task 1/file_cd.bat
@@ -0,0 +1,17 @@
+:: Creating new folders
+
+mkdir Spurs
+mkdir AVCC
+mkdir Oxford
+
+:: Navigate into a folder above
+cd Spurs
+
+:: Creating folders in mkdir Spurs
+mkdir OUFC
+mkdir Data_Analysis_is_fun
+mkdir Magpies
+
+:: Deleting two of the new folders
+rmdir Magpies
+rmdir Data_Analysis_is_fun
